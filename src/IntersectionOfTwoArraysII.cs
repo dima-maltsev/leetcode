6169e708ef0621ddcbdb4b76d45691c57c972103
Dmitry Maltsev <dmitry.maltsev@dataart.com>
2021-10-15 17:27:14 +0300

    - IntersectionOfTwoArraysII - ReverseString

--- src/IntersectionOfTwoArraysII.cs
+++ src/IntersectionOfTwoArraysII.cs
@@ -8,57 +8,102 @@ namespace LeetCode
     //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/674/
     public class IntersectionOfTwoArraysII
     {
-        public int[] Intersect(int[] nums1, int[] nums2) => Sort(nums1, nums2);
+        public int[] Intersect(int[] nums1, int[] nums2) => Counts(nums1, nums2);
 
         int[] Linq(int[] nums1, int[] nums2)
         {
-            return IntersectIterator(nums1, nums2).ToArray();
+            var list = new List<int>(nums2);
+            return nums1.Where(i => list.Remove(i)).ToArray();
         }
 
-        IEnumerable<int> IntersectIterator(int[] nums1, int[] nums2)
+        int[] BruteForce(int[] nums1, int[] nums2)
         {
-            var list = new List<int>(nums2);
+            var result = new List<int>();
+            var indices = new HashSet<int>();
 
-            foreach (var i in nums1)
+            foreach (var nums1Val in nums1)
             {
-                if (list.Remove(i))
-                    yield return i;
+                for (var j = 0; j < nums2.Length; j++)
+                {
+                    if (nums1Val != nums2[j] || indices.Contains(j)) continue;
+
+                    result.Add(nums2[j]);
+                    indices.Add(j);
+                    break;
+                }
             }
+
+            return result.ToArray();
         }
 
         int[] Sort(int[] nums1, int[] nums2)
         {
-            var map = new Dictionary<int, int>();
+            var result = new List<int>();
 
-            var arr1 = nums1;
-            var arr2 = nums2;
+            Array.Sort(nums1);
+            Array.Sort(nums2);
 
-            if (nums2.Length < nums1.Length)
+            var i = 0;
+            var j = 0;
+
+            while (i < nums1.Length && j < nums2.Length)
             {
-                arr1 = nums2;
-                arr2 = nums1;
-            }
-
-            Array.Sort(arr2);
-
-            for (var i = 0; i < arr1.Length; i++)
-            {
-                var index = Array.BinarySearch(arr2, arr1[i]);
-                if (index >= 0)
+                if (nums1[i] < nums2[j])
                 {
-                    map.Add(arr1[i], i);
+                    i++;
+                }
+                else if (nums1[i] > nums2[j])
+                {
+                    j++;
+                }
+                else
+                {
+                    result.Add(nums1[i]);
+                    i++;
+                    j++;
                 }
             }
 
-            return map.Keys.ToArray();
+            return result.ToArray();
+        }
+
+        int[] Counts(int[] nums1, int[] nums2)
+        {
+            var result = new List<int>();
+            var counts = new Dictionary<int, int>();
+
+            foreach (var val in nums1)
+            {
+                if (counts.ContainsKey(val))
+                {
+                    counts[val]++;
+                }
+                else
+                {
+                    counts.Add(val, 1);
+                }
+            }
+
+            foreach (var val in nums2)
+            {
+                if (counts.ContainsKey(val) && counts[val] > 0)
+                {
+                    result.Add(val);
+                    counts[val]--;
+                }
+            }
+
+            return result.ToArray();
         }
 
         [Theory]
+        [InlineData(new[] { 1, 2, 2, 1 }, new[] { 2 }, new[] { 2 })]
         [InlineData(new[] { 1, 2, 2, 1 }, new[] { 2, 2 }, new[] { 2, 2 })]
         [InlineData(new[] { 4, 9, 5 }, new[] { 9, 4, 9, 8, 4 }, new[] { 4, 9 })]
         public void Test(int[] nums1, int[] nums2, int[] expected)
         {
             var actual = Intersect(nums1, nums2);
+            Array.Sort(actual);
             Assert.Equal(expected, actual);
         }
     }
