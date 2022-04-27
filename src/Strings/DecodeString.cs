c26b8a12eb11dbbf8bfd4044b8fc08978b75f0d5
Dmitry Maltsev <dmitry.maltsev@dataart.com>
2022-04-27 21:38:34 +0300

    DecodeString

--- src/Strings/DecodeString.cs
+++ src/Strings/DecodeString.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Xunit;
 
 namespace LeetCode;
@@ -61,6 +63,82 @@ public class DecodeString
 
         return result;
     }
+
+    string DecodeFaster(string s)
+    {
+        var stack = new Stack<string>();
+        var currStr = "";
+        var currNum = "";
+        
+        foreach (var c in s)
+        {
+            switch (c)
+            {
+                case '[':
+                    stack.Push(currStr);
+                    stack.Push(currNum);
+                    currStr = "";
+                    currNum = "";
+                    break;
+                case ']':
+                {
+                    var prevNum = int.Parse(stack.Pop());
+                    var prevStr = stack.Pop();
+                    currStr = prevStr + string.Concat(Enumerable.Repeat(currStr, prevNum));
+                    break;
+                }
+                default:
+                {
+                    if (char.IsDigit(c))
+                    {
+                        currNum += c;
+                    }
+                    else
+                    {
+                        currStr += c;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return currStr;
+    }
+
+    string DecodeRecursive(string s)
+    {
+        var i = 0;
+        return SubDecode(s, ref i);
+    }
+
+    string SubDecode(string s, ref int i)
+    {
+        var builder = new StringBuilder();
+
+        while (i < s.Length && s[i] != ']')
+        {
+            if (char.IsDigit(s[i]))
+            {
+                var num = 0;
+                while (i < s.Length && char.IsDigit(s[i]))
+                    num = num * 10 + (s[i++] - '0');
+
+                i++; // '['
+                var innerStr = SubDecode(s, ref i);
+                i++; // ']'
+
+                while (num-- > 0)
+                    builder.Append(innerStr);
+            }
+            else
+            {
+                builder.Append(s[i++]);
+            }
+        }
+
+        return builder.ToString();
+    }
     
     [Theory]
     [InlineData("3[a]2[bc]", "aaabcbc")]
@@ -68,7 +146,7 @@ public class DecodeString
     [InlineData("2[abc]3[cd]ef", "abcabccdcdcdef")]
     public void Test(string input, string expected)
     {
-        var actual = Decode(input);
+        var actual = DecodeFaster(input);
         Assert.Equal(expected, actual);
     }
 }
